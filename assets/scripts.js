250925e8f6fd7231a8cc46c2cfb9a1534f5644f5
Casi Clarkson <casiclarkson@gmail.com>
2024-03-20 16:01:23 -0700

    changes to js and html

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,26 @@
 
-/*
-if (topic === 'HTML') {
+
+const topics= ["HTML", "CSS", "GIT", "Javascript"];
+const randomTopic = topics[Math.floor(Math.random()* topics.length)];
+function listTopics() {
+  for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+}
+function selectTopic() {
+if (randomTopic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (randomTopic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
-*/
-const topics= ["HTML", "CSS", "GIT", "Javascript"];
-for(let x = 0; x < topics.length; x++) {
- console.log(topics[x]);
 }
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
